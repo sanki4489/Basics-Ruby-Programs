b1f2704957ae18ee32bd79fda9bc8388a045aae8
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-06-06 09:33:56 +0530

    Create Basic_info.rb

---
+++ Basic_info.rb
@@ -0,0 +1,12 @@
+print "What's your first name?"
+first_name = gets.chomp.capitalize!
+puts "your first name #{first_name}"
+print "What's your last name?"
+last_name = gets.chomp.capitalize!
+puts "your first name #{last_name}"
+print "Enter your city"
+city = gets.chomp.capitalize!
+puts "your city #{city}"
+print "Enter you r state"
+state = gets.chomp.upcase!
+puts "your state is #{state}"
