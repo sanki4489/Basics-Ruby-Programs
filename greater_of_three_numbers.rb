414e56674c506818d9a07b13d4c61eba6c527e30
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-06-06 10:12:49 +0530

    Create greater_of_three_numbers.rb

---
+++ greater_of_three_numbers.rb
@@ -0,0 +1,13 @@
+#program to see greater of three
+a=3
+b=4 
+c=5
+if a>b && a>c
+  print "a is the greatest"
+elsif b>a && b>c
+  print "b is the greatest"
+elsif c>a && c>b
+  print "c is the greatest one"
+else
+  print "enter a valid number"
+end
