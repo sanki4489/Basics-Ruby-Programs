cfe7090ada268e6922ae4107d40132f009434f80
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-06-07 15:34:03 +0530

    Update histogram.rb

--- histogram.rb
+++ histogram.rb
@@ -7,3 +7,20 @@ words.each { |word| frequencies[word] += 1 }
 frequencies = frequencies.sort_by {|a, b| b }
 frequencies.reverse!
 frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+
+
+
+#my code
+puts "your sentence: "
+text = gets.chomp
+words = text.split
+frequencies = Hash.new(0)
+ words.each do |word|
+   frequencies[word]+=1
+ end  
+ frequencies = frequencies.sort_by do |word, number|
+   number
+ end
+frequencies.each {|word, number|
+  puts word + " " + number.to_s
+} 
